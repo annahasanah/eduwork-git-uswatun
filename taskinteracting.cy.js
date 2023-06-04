8d85af8b9a9c0fc4b0d299dc7668e1f299639219
annahasanah <uswatunhasanahpersonal@gmail.com>
2023-06-04 11:33:40 +0700

    mengubah taskinterasting.cy.js

--- taskinteracting.cy.js
+++ taskinteracting.cy.js
@@ -1,12 +1,7 @@
 describe('login-form', () => {
     it('should keep me sign in', () => {
-        cy.visit('http://zero.webappsecurity.com/signin.html', {timeout : 10000})
-        cy.url().should('include', 'signin.html')
-    
-    })
-
-    it('should on click label', () => {
-        cy.get('.btn').click()
-        cy.get('checkbox').contains('user-remember-me')
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+        cy.get("#user_remember_me").check()
     });
 });
