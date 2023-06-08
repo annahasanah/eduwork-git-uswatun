154d18ca1d755f2e69271106beeb1ce35ad78ebe
annahasanah <uswatunhasanahpersonal@gmail.com>
2023-06-08 15:32:19 +0700

    menambahkan cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    e2e: {
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+      },
+    },
+  };
+  module.exports ={
+    browser: 'chrome'
+    // cypress akan menggunaakan browser chrome untuk menjalankan pengujian
+  };
